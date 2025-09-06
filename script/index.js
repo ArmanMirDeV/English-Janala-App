0d5a6b5f418eaa3db0a4dfd5e1bcdda5b9f66b8f
Arman Mir <mirarman8583@gmail.com>
2025-09-06 21:33:32 +0600

    Load Words by level and display cards

--- script/index.js
+++ script/index.js
@@ -4,6 +4,29 @@ const loadLessons = () => {
         .then(json => displayLessons(json.data));
 }
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data))
+};
+
+const displayLevelWord = (words) =>{
+    const wordContainer = document.getElementById("word-container");
+    // wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+         <p> cat </p>
+        `;
+        wordContainer.append(card)
+    })
+}
+
+
 const displayLessons = (lessons) => {
 
     // 1. get the container & empty
@@ -19,7 +42,7 @@ const displayLessons = (lessons) => {
         console.log(lesson)
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button  class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open"></i>
+        <button onclick="loadLevelWord(${(lesson.level_no)})"  class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open"></i>
         Lesson - ${lesson.level_no} </button>
         
         `;
@@ -31,6 +54,6 @@ const displayLessons = (lessons) => {
         levelContainer.append(btnDiv);
     }
 
-    x
+    
 };
 loadLessons()
