e14b364b5cb7d36b790ccd51a462b982842802cb
Arman Mir <mirarman8583@gmail.com>
2025-09-07 15:36:09 +0600

    Speak Vocabulary added

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
+
+
 const createElements = (arr) => {
     const htmElements = arr.map(el => `<span class="btn">${el} </span>`);
     return (htmElements.join(" "));
@@ -136,7 +144,7 @@ const displayLevelWord = (words) => {
             </div>
             <div class="flex justify-between items-center ">
                 <button onclick="loadWordDetail(${word.id})"  class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
 
 
